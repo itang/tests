9edef2a02b289c780b612be60dee909f0067c752
itang <live.tang@gmail.com>
2014-08-28 14:02:23 +0800

    u

--- test-akka-scala/src/main/scala/example.scala
+++ test-akka-scala/src/main/scala/example.scala
@@ -1,5 +1,7 @@
 package example
 
+import scala.language.postfixOps
+
 import java.util.Date
 
 import scala.concurrent._
@@ -9,29 +11,34 @@ import duration._
 import scatang._
 
 import akka.actor.{ ActorSystem, Props, Actor, ActorRef }
+import akka.pattern.ask
+import akka.util.Timeout
 
 sealed trait Cmd
 case object StopActor extends Cmd
 case object StopSystem extends Cmd
 case class Message(msg: String) extends Cmd
+case object GetThreads extends Cmd
 
 object Example {
 
   class MyActor extends Actor {
     var count = 0;
+    var map = scala.collection.mutable.HashMap[Long, Long]()
     override def receive = {
       case StopSystem => {
-        println("receive exit system message")
+        println(s"receive exit system message, message total:$count")
         context.system.shutdown()
       }
       case StopActor => {
         println("receive stop the actor message")
         context.stop(self)
       }
+      case GetThreads => this.sender ! map
       case Message(msg) => {
+        val tid = Thread.currentThread().getId()
+        map += (tid -> (map.getOrElse(tid, 0L) + 1L))
         count += 1
-        if (count > 9990)
-          println(msg)
       }
     }
   }
@@ -47,7 +54,7 @@ object Example {
 
     val f = Future { scala.io.StdIn.readLine }
     try {
-      Await.result(f, 3.seconds)
+      Await.result(f, 1.seconds)
     } catch {
       case e: Exception => {
         println(e.getMessage())
@@ -56,8 +63,16 @@ object Example {
     }
 
     time {
-      for (i <- (0 until 10000)) {
+      for (i <- (0 until 10000000)) {
         actor ! Message(new Date().toString())
+      }
+
+      implicit val timeout = Timeout(5 seconds)
+      val future = actor ? GetThreads // enabled by the “ask” import
+      val result = Await.result(future, 2.seconds).asInstanceOf[scala.collection.mutable.HashMap[Long, Long]]
+      println(s"use threads: ${result.size}")
+      for ((k, v) <- result) {
+        println(s"thread-$k, messages: $v")
       }
 
       actor ! StopSystem
