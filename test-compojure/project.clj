0976df0460e7a03077750116065b33eb1d0b1d3a
itang <live.tang@gmail.com>
2014-09-09 21:08:50 +0800

    add hiccup test

--- test-compojure/project.clj
+++ test-compojure/project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.9"]
                  [http-kit "2.1.16"]
-                 [selmer "0.7.0"]])
+                 [selmer "0.7.0"]
+                 [hiccup "1.0.5"]])
--- test-compojure/src/test_compojure/core.clj
+++ test-compojure/src/test_compojure/core.clj
@@ -1,14 +1,15 @@
 (ns test-compojure.core
   (:require [compojure.core :refer :all]
             [org.httpkit.server :refer [run-server]]
-            [selmer.parser :refer [render-file]]))
+            [selmer.parser :refer [render-file]]
+            [hiccup.core :refer :all]))
 
 (defn hello [req]
   (render-file "templates/hello.html" {:name "itang"}))
 
 (defroutes myapp
-  (GET "/" [] "Hello World")
-(GET "/hello" [] hello))
+  (GET "/" [] (html [:body [:h1 "Hello World "] [:a {:href "/hello" :target "_blank"} "Hello"]]))
+  (GET "/hello" [] hello))
 
 (defn -main []
   (run-server myapp {:port 5000}))
