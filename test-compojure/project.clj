57c0fce431a745f9719aa30cdc934e39f6dafcc3
itang <live.tang@gmail.com>
2014-09-09 21:54:21 +0800

    update http-kit

--- test-compojure/project.clj
+++ test-compojure/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.9"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.19"]
                  [selmer "0.7.0"]
                  [hiccup "1.0.5"]])
---
+++ test-compojure/run.sh
@@ -0,0 +1,1 @@
+java -Xms2g -Xmx2g -cp `lein classpath` clojure.main -m test-compojure.core
