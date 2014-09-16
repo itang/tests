41be37a89c6eb7e4bceea4c92efe8031b29b45eb
itang <live.tang@gmail.com>
2014-09-16 10:11:43 +0800

    refactoring

--- test-java-exec/src/main/scala/ZtExec.scala
+++ test-java-exec/src/main/scala/ZtExec.scala
@@ -1,6 +1,6 @@
 package zt_exec
 
-import scala.collection.mutable.ArrayBuffer
+//import scala.collection.mutable.ArrayBuffer
 
 import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.exec.ProcessResult
@@ -8,9 +8,10 @@ import org.zeroturnaround.exec.ProcessResult
 object ZtExec {
 
   def exec(cmd: String, args: String*)(read: Boolean = false): ProcessResult = {
-    val buf = ArrayBuffer(cmd) ++= args
+    //val buf = ArrayBuffer(cmd) ++= args
+    var buf = cmd :: args.toList
 
-    val p = new ProcessExecutor().command(buf.toArray: _*)
+    val p = new ProcessExecutor().command(buf: _*)
     p.readOutput(read).execute()
   }
 }
