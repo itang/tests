1d85754facb3df4e25b6a29f2af8127c250fdffe
itang <live.tang@gmail.com>
2014-09-23 10:47:47 +0800

    more

--- test-rxjava/src/main/java/Observables.java
+++ test-rxjava/src/main/java/Observables.java
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class Observables {
 
@@ -32,11 +33,44 @@ public class Observables {
 			}
 		});
 
-		Observable.interval(1, TimeUnit.SECONDS).take(5).toBlocking()
+		Observable.interval(100, TimeUnit.MILLISECONDS).take(5).toBlocking()
 				.forEach(new Action1<Long>() {
 					@Override
 					public void call(Long counter) {
 						System.out.println("Got: " + counter);
+					}
+				});
+
+		Observable.just("1", "2", "3")
+				.flatMap(new Func1<String, Observable<Integer>>() {
+					@Override
+					public Observable<Integer> call(String id) {
+						System.out.println(Thread.currentThread());
+						return Observable.just(Integer.parseInt(id));
+					}
+				}).subscribe(new Action1<Integer>() {
+					@Override
+					public void call(Integer document) {
+						System.out.println("Got: " + document);
+					}
+				});
+
+		// Transforming Observables
+		Observable.interval(10, TimeUnit.MILLISECONDS).take(20)
+				.map(new Func1<Long, String>() {
+					@Override
+					public String call(Long input) {
+						if (input % 3 == 0) {
+							return "Fizz";
+						} else if (input % 5 == 0) {
+							return "Buzz";
+						}
+						return Long.toString(input);
+					}
+				}).toBlocking().forEach(new Action1<String>() {
+					@Override
+					public void call(String s) {
+						System.out.println(s);
 					}
 				});
 	}
