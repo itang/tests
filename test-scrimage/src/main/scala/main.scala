1ab9920a92094429e2610ba031e4b6dadc799fa7
itang <live.tang@gmail.com>
2014-09-24 17:13:24 +0800

    add fit test

--- test-scrimage/src/main/scala/main.scala
+++ test-scrimage/src/main/scala/main.scala
@@ -12,8 +12,9 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val in = new File("logo.jpg")
-    val out = new File("logo-output.png")
-    Image(in).trim(0, 0, 272 - 72, 0).write(out)
+    val out = new File("logo-output-80_80.png")
+    Image(in).trim(0, 0, 272 - 72, 0).fit(80, 80).write(out)
+    Image(out).fit(16, 16).write(new File("logo-output-16_16.png"))
   }
 }
 
