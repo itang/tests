d2c9ab114a1de67f5b103d01aa2126e0293ee351
itang <live.tang@gmail.com>
2014-09-28 16:17:40 +0800

    add iron test

---
+++ test-iron/src/main.rs
@@ -0,0 +1,15 @@
+extern crate http;
+extern crate iron;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with(status::Ok, "Hello world!"))
+}
+
+fn main() {
+    Iron::new(hello_world).listen(Ipv4Addr(127, 0, 0, 1), 3000);
+    println!("On 3000");
+}
