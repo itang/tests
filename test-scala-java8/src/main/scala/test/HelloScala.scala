934bd5fabd0facdc58d599e6a912c6fbc58aaae6
itang <live.tang@gmail.com>
2014-09-30 07:28:39 +0800

    more

--- test-scala-java8/src/main/scala/test/HelloScala.scala
+++ test-scala-java8/src/main/scala/test/HelloScala.scala
@@ -1,9 +1,24 @@
 package test
 
+import language.postfixOps
+
+import scatang._
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import duration._
+
 object HelloScala extends App {
   new Thread((() ⇒ {
+    Thread.sleep(200.milliseconds.toMillis)
     println("Hello,World (From Scala)")
   }): Runnable).start()
 
-  List("Hello", "World") map (_.toUpperCase()) foreach println
+  val future = Future {
+    List("Hello", "World") map (_.toUpperCase()) tap (_ foreach println)
+  }
+
+  Await.result(future, 1 seconds)
+
+  private def println(msg: Any): Unit = scala.Console.println(Thread.currentThread() + ": " + msg)
 }
