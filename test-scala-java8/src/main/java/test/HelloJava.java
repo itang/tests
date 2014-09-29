3f43519098ede5fd48e92d52adf75d1ae74cb537
itang <live.tang@gmail.com>
2014-09-29 15:22:10 +0800

    more java8

--- test-scala-java8/src/main/java/test/HelloJava.java
+++ test-scala-java8/src/main/java/test/HelloJava.java
@@ -1,19 +1,38 @@
 package test;
 
+import static java.util.stream.Collectors.toList;
+import static java.lang.System.out;
+
 import java.util.ArrayList;
 import java.util.List;
-import static java.util.stream.Collectors.toList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class HelloJava {
-    public static void main(String[] args) {
-        new Thread(() -> {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        go(() -> {
             System.out.println("Hello, World!(From Java)");
-        }).start();
+        });
 
         List<String> list = new ArrayList<>();
         list.add("Hello");
         list.add("World");
 
-        list.stream().map(x -> x.toUpperCase()).collect(toList()).forEach(System.out::println);
+        Future<List<String>> ret = go(() -> list.stream().map(x -> x.toUpperCase()).collect(toList()));
+        ret.get().forEach(out::println);
     }
+
+    private static void go(Runnable runnable) {
+        pool.execute(runnable);
+    }
+
+    private static <T> Future<T> go(Callable<T> callable) {
+        return pool.submit(callable);
+    }
+
+    private static ExecutorService pool = Executors.newCachedThreadPool();
 }
