098411a9969d39f5dabe14d894f4f296e31f4612
itang <live.tang@gmail.com>
2014-10-02 11:39:41 +0800

    compojure-1.2.0

--- test-compojure/project.clj
+++ test-compojure/project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.9"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [compojure "1.2.0"]
                  [http-kit "2.1.19"]
                  [selmer "0.7.1"]
                  [hiccup "1.0.5"]
                  [prone "0.6.0"]
                  #_[cheshire "5.3.1"]
-                 [lib-noir "0.8.9"]]
+                 [lib-noir "0.9.0"]]
   :main test-compojure.core)
