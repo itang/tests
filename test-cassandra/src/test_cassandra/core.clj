90e4664c487b514bc3e7608ff03c563772e3887b
itang <live.tang@gmail.com>
2014-10-13 14:10:32 +0800

    cleanup

--- test-cassandra/src/test_cassandra/core.clj
+++ test-cassandra/src/test_cassandra/core.clj
@@ -6,63 +6,59 @@
 
 (def ^:cosnt HOST "127.0.0.1")
 
-(def ^:const USER_TABLE_NAME "users")
+(def ^:const UN "users")
 
-(def ^:const USER_POSTS_TABLE_NAME "user_posts")
+(def ^:const UPN "user_posts")
 
 (def ^:const TEST_KEYSPACE "test_keyspace")
 
 (defmacro try! [& body]
   `(try [true (do ~@body)]
-        (catch Exception ~'e
-          (-> ~'e .getMessage println)
-          [false ~'e])))
+     (catch Exception ~'e
+       (-> ~'e .getMessage println)
+       [false ~'e])))
 
 (defn- init-keyspace [conn]
   (try!
     (cql/create-keyspace conn
-                         TEST_KEYSPACE
-                         (with {:replication
-                                 {:class              "SimpleStrategy"
-                                  :replication_factor 1}}))))
+      TEST_KEYSPACE
+      (with {:replication
+              {:class              "SimpleStrategy"
+               :replication_factor 1}}))))
 (defn- use-keyspace [conn]
   (cql/use-keyspace conn TEST_KEYSPACE))
 
 (defn- create-tables [conn]
-  (try! (cql/drop-table conn USER_TABLE_NAME))
-  (try! (cql/drop-table conn USER_POSTS_TABLE_NAME))
+  (try! (cql/drop-table conn UN))
+  (try! (cql/drop-table conn UPN))
 
-  (try! (cql/create-table conn USER_TABLE_NAME
-                          (column-definitions {:name        :varchar
-                                               :age         :int
-                                               :city        :varchar
-                                               :primary-key [:name]})))
-  (try! (cql/create-table conn USER_POSTS_TABLE_NAME
-                          (column-definitions {:username    :varchar
-                                               :post_id     :varchar
-                                               :body        :text
-                                               :primary-key [:username :post_id]})))
+  (try! (cql/create-table conn UN
+          (column-definitions {:name        :varchar
+                               :age         :int
+                               :city        :varchar
+                               :primary-key [:name]})))
+  (try! (cql/create-table conn UPN
+          (column-definitions {:username    :varchar
+                               :post_id     :varchar
+                               :body        :text
+                               :primary-key [:username :post_id]})))
   #_(try! (cql/delete conn USER_POSTS_TABLE_NAME))
   #_(try! (cql/delete conn USER_POSTS_TABLE_NAME)))
 
 (defn init-users-data [conn]
   (doto conn
-    (cql/insert USER_TABLE_NAME {:name "Alex" :city "Munich" :age (int 19)})
-    (cql/insert USER_TABLE_NAME {:name "Robert" :city "Berlin" :age (int 25)})
-    (cql/insert USER_TABLE_NAME {:name "Sam" :city "San Francisco" :age (int 21)})
+    (cql/insert UN {:name "Alex" :city "Munich" :age (int 19)})
+    (cql/insert UN {:name "Robert" :city "Berlin" :age (int 25)})
+    (cql/insert UN {:name "Sam" :city "San Francisco" :age (int 21)})
 
-    (cql/insert USER_POSTS_TABLE_NAME {:username "Alex" :post_id "post1" :body "first post body"})
-    (cql/insert USER_POSTS_TABLE_NAME {:username "Alex" :post_id "post2" :body "second post body"})
-    (cql/insert USER_POSTS_TABLE_NAME {:username "Alex" :post_id "post3" :body "third post body"})))
+    (cql/insert UPN {:username "Alex" :post_id "post1" :body "first post body"})
+    (cql/insert UPN {:username "Alex" :post_id "post2" :body "second post body"})
+    (cql/insert UPN {:username "Alex" :post_id "post3" :body "third post body"})))
 
 (defn print-data [conn]
-  (doseq [user (cql/select conn USER_TABLE_NAME (where :name [:in ["Alex" "Robert"]]))]
+  (doseq [user (cql/select conn UN (where :name [:in ["Alex" "Robert"]]))]
     (println user))
-  (doseq [post (cql/select conn USER_POSTS_TABLE_NAME
-                           (columns :post_id)
-                           (where :username "Alex"
-                                  :post_id [> "post1"]
-                                  :post_id [< "post3"]))]
+  (doseq [post (cql/select conn UPN (columns :post_id) (where :username "Alex" :post_id [> "post1"] :post_id [< "post3"]))]
     (println post)))
 
 (defn -main
