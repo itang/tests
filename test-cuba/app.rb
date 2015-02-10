9c0af03113450a629317a404906a211a7f0275a2
itang <live.tang@gmail.com>
2015-02-10 11:06:20 +0800

    test cuba on torquebox + jruby-9000

---
+++ test-cuba/app.rb
@@ -0,0 +1,18 @@
+# cat hello_world.rb
+require "cuba"
+require "rack/protection"
+
+Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
+Cuba.use Rack::Protection
+
+Cuba.define do
+  on get do
+    on "hello" do
+      res.write "Hello world!"
+    end
+
+    on root do
+      res.redirect "/hello"
+    end
+  end
+end
