9aed2529641a16b78e037985a1ac076fb0ce4e8e
itang <live.tang@gmail.com>
2015-02-12 12:47:29 +0800

    java 8

--- test-buildr/src/main/java/Main.java
+++ test-buildr/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
+
+    Thread t = new Thread(()-> System.out.println("Hello, from:" + Thread.currentThread()));
+    t.start();
+    t.join();
   }
 }
