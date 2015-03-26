d21a4b3df397eaaacda91acb702cf5dff5a42791
itang <live.tang@gmail.com>
2015-03-26 11:45:05 +0800

    add crystal-mysql test

---
+++ test-crystal-mysql/src/main.cr
@@ -0,0 +1,47 @@
+require "mysql"
+
+module Logger
+  extend self
+
+  def info(msg)
+    puts "INFO-#{Time.now}: #{msg}"
+  end
+
+  def error(msg)
+    STDERR.puts "ERROR-#{Time.now}: #{msg}"
+  end
+end
+include Logger
+
+module Resources
+  extend self
+  def with_resource(res)
+    begin
+      yield(res)
+    rescue e: Exception
+      raise e
+    ensure
+      begin
+        res.close()
+      rescue e1: Exception
+        error e1
+      end
+    end
+  end
+end
+include Resources
+
+
+begin
+  # MySQL.connect(host, user, password, database, port, socket, flags = 0)
+  with_resource(MySQL.connect("127.0.0.1", "root", "root1", "mydb", 3306_u16, nil)) do |conn|
+
+    info(conn.query(%{SELECT 1}))
+
+    rows = conn.query(%{select id, name from account})
+
+    info(rows)
+  end
+rescue e: Exception
+  error(e)
+end
