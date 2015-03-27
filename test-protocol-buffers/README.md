2a0b2820b63b96956e793823c3fa5725cc9eb6b8
itang <live.tang@gmail.com>
2015-03-27 14:17:02 +0800

    test spring-boot protobufs

---
+++ test-protocol-buffers/README.md
@@ -0,0 +1,1 @@
+@see https://spring.io/blog/2015/03/22/using-google-protocol-buffers-with-spring-mvc-based-rest-services
---
+++ test-protocol-buffers/src/main/ruby/client.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require './customer.pb'
+require 'net/http'
+require 'uri'
+
+uri = URI.parse('http://localhost:8080/customers/3')
+body = Net::HTTP.get(uri)
+puts Demo::Customer.parse(body)
+
