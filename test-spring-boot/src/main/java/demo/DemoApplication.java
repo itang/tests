228442b856dd52a49abcedc35cc43b15e3d3dfd1
itang <live.tang@gmail.com>
2015-04-07 10:37:05 +0800

    test spring-boot with undertow

---
+++ test-spring-boot/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
