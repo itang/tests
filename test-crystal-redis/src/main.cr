61419249053bc4a0fcf163dc76faefe7819d3a04
itang <live.tang@gmail.com>
2015-05-01 11:19:07 +0800

    update

--- test-crystal-redis/src/main.cr
+++ test-crystal-redis/src/main.cr
@@ -1,9 +1,22 @@
 require "redis"
+require "crtang"
+
+include Crtang
 
 Redis.open("127.0.0.1", 6379) do |client|
- 10.times do 
-    client.set("now", Time.new)
-    p client.get("now")
-    sleep(1)
+  Crtang.time do
+    10.times do
+      client.set("now", Time.new)
+      p client.get("now")
+      sleep(0.2)
+    end
+  end
+
+  time do
+    (1..100000).each do |x|
+      key = "hello.#{x}"
+      client.set key, x
+      #p client.get key
+    end
   end
 end
