9c6eb511012f71fd8ff64d97c487724f842da12c
itang <live.tang@gmail.com>
2015-05-03 13:28:29 +0800

    scala handlebars

--- test-spring-boot/src/main/scala/demo/controllers/TestScalaController.scala
+++ test-spring-boot/src/main/scala/demo/controllers/TestScalaController.scala
@@ -1,17 +1,30 @@
 package demo.controllers
 
-import org.springframework.stereotype.Controller
+import java.util.Date
 
-import demo.ext.BaseController;
+import scala.collection.JavaConverters._
+
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ ResponseBody, RequestMapping }
+import org.springframework.ui.ModelMap
+
+import demo.ext.BaseController
 
 @Controller
-@RequestMapping(Array("/scala"))
+@RequestMapping(Array("/test/scala"))
 class TestScalaController extends BaseController {
 
   @RequestMapping(Array(""))
   @ResponseBody
   def index(): String = {
-    s"V21: Hello, from scala (at ${new java.util.Date()})"
+    s"V21: Hello, from scala (at ${new Date()})"
+  }
+
+  @RequestMapping(Array("/handlebars"))
+  def index(model: ModelMap): String = {
+    model.put("now", new Date)
+    model.put("list", List("a", "b", "c").asJava)
+
+    "/test/scala/handlebars"
   }
 }
