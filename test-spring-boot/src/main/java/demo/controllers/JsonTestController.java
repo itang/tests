239bb0f958091c1cabf16a0bddb0f8a2b0bfaa95
itang <live.tang@gmail.com>
2015-05-03 11:43:18 +0800

    handlebars json

---
+++ test-spring-boot/src/main/java/demo/controllers/JsonTestController.java
@@ -0,0 +1,41 @@
+package demo.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Lists;
+
+@Controller
+@RequestMapping("/test/json")
+public class JsonTestController {
+
+    @RequestMapping("")
+    public String index(ModelMap model) {
+        User user = new User("itang");
+        model.put("user", user);
+
+        return "/test/json/index";
+    }
+
+    @RequestMapping("/data")
+    @ResponseBody
+    public List<User> data() {
+        return Lists.newArrayList(new User("Itang"), new User("Tqibm"));
+    }
+
+    public static class User {
+        private final String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
