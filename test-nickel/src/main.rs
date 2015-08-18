c2025e93a6ba32dd16ae20e1f385dd7e91063817
itang <live.tang@gmail.com>
2015-08-18 21:53:39 +0800

    cleanup

--- test-nickel/src/main.rs
+++ test-nickel/src/main.rs
@@ -5,12 +5,20 @@ use nickel::mimes::MediaType;
 use nickel::Nickel;
 use nickel::router::http_router::HttpRouter;
 
+mod hello {
+    pub fn hello(name: String) {
+        println!("Hello, {}", name);
+    }
+}
+
 fn main() {
+    hello::hello("itang".to_string());
+
     let mut server = Nickel::new();
     server.get("/hello", middleware!("Hello, world!"));
     server.get("/test", middleware! { |_, mut res|
         res.set(MediaType::Json);
         r#"{"foo":"bar"}"#
-        });
+    });
     server.listen("127.0.0.1:6767");
 }
