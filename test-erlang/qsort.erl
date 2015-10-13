39270e862eb5c3484f35c5a8c5187b462f1a377d
itang <live.tang@gmail.com>
2015-10-13 09:47:47 +0800

    elixir version

--- test-erlang/qsort.erl
+++ test-erlang/qsort.erl
@@ -5,3 +5,9 @@ qsort([]) -> [];
 qsort([P|T]) -> qsort([X || X <-T, X < P]) ++ [P] ++ qsort([X || X <- T, X >= P]).
 
 %% qsort([3,2,1]). => qsort([2,1]) ++ [3] ++ qsort([]).
+
+%% Elixir version. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% defmodule Hello do
+%%   def qsort([]), do: []
+%%   def qsort([h|t]), do: qsort(for x <-t, x < h, do: x) ++ [h] ++ qsort(for x <- t, x >= h, do: x)
+%% end
