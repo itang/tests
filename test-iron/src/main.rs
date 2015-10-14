4fe715889d61a7208557a7b5a213b84cff40a743
itang <live.tang@gmail.com>
2015-10-14 17:39:33 +0800

    after cargo do fmt (rustfmt)

--- test-iron/src/main.rs
+++ test-iron/src/main.rs
@@ -6,27 +6,27 @@ use iron::prelude::*;
 use iron::status;
 
 fn main() {
-  let _h = _hostname(); // TODO: String => &'a str
-  let port = _port();
-  println!("INFO: hostname:{}, port: {}", _h, port);
+    let _h = _hostname(); // TODO: String => &'a str
+    let port = _port();
+    println!("INFO: hostname:{}, port: {}", _h, port);
 
-  Iron::new(|_: &mut Request| {
-    Ok(Response::with((status::Ok, "Hello world!")))
-  }).http(("localhost", port)).unwrap();
+    Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
+        .http(("localhost", port))
+        .unwrap();
 }
 
 fn _port() -> u16 {
-  let key = "PORT";
-  match env::var(key) {
-    Ok(val) => val.parse::<u16>().ok().expect("error"),
-    Err(_) => 8080
-  }
+    let key = "PORT";
+    match env::var(key) {
+        Ok(val) => val.parse::<u16>().ok().expect("error"),
+        Err(_) => 8080,
+    }
 }
 
 fn _hostname() -> String {
-  let key = "HOST";
-  match env::var(key) {
-    Ok(val) => val,
-    Err(_) => "localhost".to_string()
-  }
+    let key = "HOST";
+    match env::var(key) {
+        Ok(val) => val,
+        Err(_) => "localhost".to_string(),
+    }
 }
