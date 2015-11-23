3676adf1581cdbd5bb8fcd4b933ed3545494a896
itang <live.tang@gmail.com>
2015-11-23 15:29:23 +0800

    cleanup

--- test-storm/storm-in-action/src/main/scala/hello/lanuch.scala
+++ test-storm/storm-in-action/src/main/scala/hello/lanuch.scala
@@ -6,54 +6,28 @@ import util.Lanuch
 import backtype.storm.tuple.Fields
 import backtype.storm.topology.BoltDeclarer
 
-object HelloTopologyLanuch extends Lanuch {
+case class Parallelism(parallelism_hint: Int = 1, num_tasks: Int = 1)
+
+case class LauchConf(spout: Parallelism, sayBolt: Parallelism, counterBolt: Parallelism)
+
+abstract class BaseTopologyLauch(lauchConf: LauchConf, grouping: (String, BoltDeclarer) => BoltDeclarer) extends Lanuch {
 
   override def apply(name: String, remote: Boolean): Unit = {
     // 实例化topologyBuilder
     val builder = topologyBuilder()
 
     // 设置Spout 组件
-    val spout_parallelism_hint = 1
+    val spout_parallelism_hint = lauchConf.spout.parallelism_hint;
     val spoutName = s"${name}_spout"
     builder.setSpout(spoutName, new HelloSpout, spout_parallelism_hint)
 
     // 设置Bolt 组件
-    val say_hello_bolt_parallelism_hint = 2
+    val say_hello_bolt_parallelism_hint = lauchConf.sayBolt.parallelism_hint
     val boltSayHello = s"${name}_bolt_sayhello"
     builder.setBolt(boltSayHello, new SayHelloBolt, say_hello_bolt_parallelism_hint)
-      .shuffleGrouping(spoutName).setNumTasks(say_hello_bolt_parallelism_hint * 2)
+      .shuffleGrouping(spoutName).setNumTasks(lauchConf.sayBolt.num_tasks)
 
-    builder.setBolt(s"${name}_bolt_counthello", new CounterHelloBolt, 2).shuffleGrouping(boltSayHello)
-
-    // 构造topology 实例
-    val topology = builder.createTopology()
-
-    // 构造配置对象
-    val conf = new Config();
-    //conf.setDebug(true);
-
-    submitTopology(name, remote, conf, topology)
-  }
-}
-
-object HelloFieldsGroupingTopologyLanuch extends Lanuch {
-
-  override def apply(name: String, remote: Boolean): Unit = {
-    // 实例化topologyBuilder
-    val builder = topologyBuilder()
-
-    // 设置Spout 组件
-    val spout_parallelism_hint = 1
-    val spoutName = s"${name}_spout"
-    builder.setSpout(spoutName, new HelloSpout, spout_parallelism_hint)
-
-    // 设置Bolt 组件
-    val say_hello_bolt_parallelism_hint = 2
-    val boltSayHello = s"${name}_bolt_sayhello"
-    builder.setBolt(boltSayHello, new SayHelloBolt, say_hello_bolt_parallelism_hint).fieldsGrouping(spoutName, new Fields("word"))
-      .setNumTasks(say_hello_bolt_parallelism_hint * 2)
-
-    builder.setBolt(s"${name}_bolt_counthello", new CounterHelloBolt, 1).shuffleGrouping(boltSayHello)
+    grouping(spoutName, builder.setBolt(s"${name}_bolt_counthello", new CounterHelloBolt, lauchConf.counterBolt.parallelism_hint))
 
     // 构造topology 实例
     val topology = builder.createTopology()
@@ -66,62 +40,19 @@ object HelloFieldsGroupingTopologyLanuch extends Lanuch {
   }
 }
 
-object HelloGlobalGroupingTopologyLanuch extends Lanuch {
+object HelloTopologyLanuch extends BaseTopologyLauch(
+  LauchConf(Parallelism(1), Parallelism(2, 4), Parallelism(2, 2)),
+  (name, dc) => dc.shuffleGrouping(name))
 
-  override def apply(name: String, remote: Boolean): Unit = {
-    // 实例化topologyBuilder
-    val builder = topologyBuilder()
+object HelloFieldsGroupingTopologyLanuch extends BaseTopologyLauch(
+  LauchConf(Parallelism(1), Parallelism(2, 4), Parallelism(1, 1)),
+  (name, dc) => dc.globalGrouping(name))
 
-    // 设置Spout 组件
-    val spout_parallelism_hint = 1
-    val spoutName = s"${name}_spout"
-    builder.setSpout(spoutName, new HelloSpout, spout_parallelism_hint)
+object HelloGlobalGroupingTopologyLanuch extends BaseTopologyLauch(
+  LauchConf(Parallelism(1), Parallelism(2, 4), Parallelism(1, 1)),
+  (name, dc) => dc.globalGrouping(name))
 
-    // 设置Bolt 组件
-    val say_hello_bolt_parallelism_hint = 2
-    val boltSayHello = s"${name}_bolt_sayhello"
-    builder.setBolt(boltSayHello, new SayHelloBolt, say_hello_bolt_parallelism_hint).globalGrouping(spoutName)
-      .setNumTasks(say_hello_bolt_parallelism_hint * 2)
+object HelloAllGroupingTopologyLanuch extends BaseTopologyLauch(
+  LauchConf(Parallelism(1), Parallelism(2, 4), Parallelism(1, 1)),
+  (name, dc) => dc.allGrouping(name))
 
-    builder.setBolt(s"${name}_bolt_counthello", new CounterHelloBolt, 1).shuffleGrouping(boltSayHello)
-
-    // 构造topology 实例
-    val topology = builder.createTopology()
-
-    // 构造配置对象
-    val conf = new Config();
-    //conf.setDebug(true);
-
-    submitTopology(name, remote, conf, topology)
-  }
-}
-
-object HelloAllGroupingTopologyLanuch extends Lanuch {
-
-  override def apply(name: String, remote: Boolean): Unit = {
-    // 实例化topologyBuilder
-    val builder = topologyBuilder()
-
-    // 设置Spout 组件
-    val spout_parallelism_hint = 1
-    val spoutName = s"${name}_spout"
-    builder.setSpout(spoutName, new HelloSpout, spout_parallelism_hint)
-
-    // 设置Bolt 组件
-    val say_hello_bolt_parallelism_hint = 2
-    val boltSayHello = s"${name}_bolt_sayhello"
-    builder.setBolt(boltSayHello, new SayHelloBolt, say_hello_bolt_parallelism_hint).allGrouping(spoutName)
-      .setNumTasks(say_hello_bolt_parallelism_hint * 2)
-
-    builder.setBolt(s"${name}_bolt_counthello", new CounterHelloBolt, 1).shuffleGrouping(boltSayHello)
-
-    // 构造topology 实例
-    val topology = builder.createTopology()
-
-    // 构造配置对象
-    val conf = new Config();
-    //conf.setDebug(true);
-
-    submitTopology(name, remote, conf, topology)
-  }
-}
