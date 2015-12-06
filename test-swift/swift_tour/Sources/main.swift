a82d799c7c07c168d7cec605afeedeeb122a38ac
itang <live.tang@gmail.com>
2015-12-06 20:07:45 +0800

    finish swift tour

--- test-swift/swift_tour/Sources/main.swift
+++ test-swift/swift_tour/Sources/main.swift
@@ -241,8 +241,321 @@ func functions_closures() {
   let mappedNumbers2 = numbers.map { number in 3 * number }
   print(mappedNumbers2)
   print(numbers.map{ it in 3 * it })
+
+  let sortedNumbers = numbers.sort { $0 > $1 }
+  print(sortedNumbers)
+}
+
+func objects_classes() {
+  class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+      return "A shape with \(numberOfSides) sides."
+    }
+  }
+
+  let shape = Shape()
+  print(shape.simpleDescription())
+
+  shape.numberOfSides = 7
+  let shapeDescription = shape.simpleDescription()
+  print(shapeDescription)
+
+
+  class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+      self.name = name
+    }
+
+    func simpleDescription() -> String {
+      return "A shape \(name) with \(numberOfSides) slides."
+    }
+  }
+
+  let namedShape = NamedShape(name: "tang")
+  print(namedShape.simpleDescription())
+
+  class Square: NamedShape {
+    var sideLength: Double
+
+    init(sideLength: Double, name: String) {
+      self.sideLength = sideLength
+      super.init(name: name)
+      numberOfSides = 4
+    }
+
+    func area() -> Double {
+      return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+      return "A square with sides of length \(sideLength)."
+    }
+  }
+
+  let test = Square(sideLength: 5.2, name: "my test square")
+  let area = test.area()
+  let desc = test.simpleDescription()
+  print(area, desc)
+
+  class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+      self.sideLength = sideLength
+      super.init(name: name)
+      numberOfSides = 3
+    }
+
+    var perimeter: Double {
+      get {
+        return 3.0 * sideLength
+      }
+
+      set {
+        sideLength = newValue / 3.0
+      }
+    }
+
+    override func simpleDescription() -> String {
+      return "An equilateral triangle with sides of length \(sideLength)."
+    }
+  }
+
+  let triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+  print(triangle.perimeter)
+  triangle.perimeter = 9.9
+  print(triangle.sideLength)
+
+  class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+      willSet {
+        print("triangle willSet \(newValue)")
+        square.sideLength = newValue.sideLength
+      }
+    }
+
+    var square: Square {
+      willSet {
+        print("square willSet \(newValue)")
+        triangle.sideLength = newValue.sideLength
+      }
+    }
+
+    init(size: Double, name: String) {
+      square = Square(sideLength: size, name: name)
+      triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+  }
+
+  print("////////////////////")
+  let triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+  print(triangleAndSquare.square.sideLength)
+  print(triangleAndSquare.triangle.sideLength)
+  triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+  print(triangleAndSquare.triangle.sideLength)
+
+  let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+  let sideLength = optionalSquare?.sideLength
+  print(sideLength)
+}
+
+func enumerations_and_structures() {
+  enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+
+    func simpleDescription() -> String {
+      switch self {
+        case .Ace:
+          return "ace"
+        case .Jack:
+          return "jack"
+        case .Queen:
+          return "queen"
+        case .King:
+          return "kind"
+        default:
+          return String(self.rawValue)
+      }
+    }
+  }
+
+  let ace = Rank.Ace
+  let aceRawValue = ace.rawValue
+  print(aceRawValue)
+
+  if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+  }
+
+  enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+      switch self {
+        case .Spades:
+          return "spades"
+        case .Hearts:
+          return "hearts"
+        case .Diamonds:
+          return "diamonds"
+        case .Clubs:
+          return "clubs"
+      }
+    }
+  }
+
+  let hearts = Suit.Hearts
+  print(hearts.simpleDescription())
+
+  // One of the most important differences between structures and classes is that structures are always copied when they are passed around in your code,
+  // but classes are passed by reference
+  struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+      return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+  }
+
+  let threeOfSpades = Card(rank: .Three, suit: .Spades)
+  print(threeOfSpades.simpleDescription())
+
+  enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+  }
+
+  let success = ServerResponse.Result("6:00 am", "8:09 pm")
+  let failure = ServerResponse.Error("Out of cheese.")
+
+  func print_result(resp: ServerResponse) {
+    switch resp {
+      case let .Result(sunrise, sunset):
+         print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+      case let .Error(error):
+        print("Failture... \(error)")
+    }
+  }
+
+  print_result(success)
+  print("/////")
+  print_result(failure)
+}
+
+protocol ExampleProtocol {
+  var simpleDescription: String { get }
+  mutating func adjust()
+}
+
+extension Int: ExampleProtocol {
+  var simpleDescription: String {
+    return "The number \(self)"
+  }
+
+  mutating func adjust(){
+    self += 42
+  }
+
+  func times(block: (Int) -> Void) -> Void {
+    for var i = 0; i < self; ++i {
+      block(i)
+    }
+  }
+}
+
+func protocols_and_extensions() {
+  class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+      simpleDescription += "  Now 100% adjusted."
+    }
+  }
+
+  let a = SimpleClass()
+  a.adjust()
+  let aDescription = a.simpleDescription
+  print(aDescription)
+
+  struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+      simpleDescription += " (adjusted)"
+    }
+  }
+
+  var b = SimpleStructure()
+  b.adjust()
+
+  let bDescription = b.simpleDescription
+  print(bDescription)
+
+  print(7.simpleDescription)
+
+  var i = 7
+  i.adjust()
+  print(i.simpleDescription)
+
+  let protocolValue: ExampleProtocol = i
+  print(protocolValue.simpleDescription)
+
+  10.times { x in
+    print(x)
+  }
+
+  5.times { print($0) }
+}
+
+func generics() {
+  func repeatItem<Item>(item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+      result.append(item)
+    }
+    return result
+  }
+
+  let items = repeatItem("knock", numberOfTimes: 4)
+  print(items)
+
+  enum OptionalValue<Wrapper> {
+    case None
+    case Some(Wrapper)
+  }
+
+  var possibleInteger: OptionalValue<Int> = .None
+  print(possibleInteger)
+
+  possibleInteger = .Some(100)
+  print(possibleInteger)
+
+
+  func anyCommonElements<T: SequenceType, U: SequenceType where T.Generator.Element: Equatable,
+    T.Generator.Element == U.Generator.Element> (lhs: T, _ rhs: U) -> Bool {
+    for lhsItem in lhs {
+      for rhsItem in rhs {
+        if lhsItem == rhsItem {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  let r = anyCommonElements([1, 2, 3], [3])
+  print(r)
 }
 
 simple_values()
 control_flow()
 functions_closures()
+objects_classes()
+enumerations_and_structures()
+protocols_and_extensions()
+generics()
