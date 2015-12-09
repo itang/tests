c7cd09cb54d4bd5829761751443cc0a77a2c9074
itang <live.tang@gmail.com>
2015-12-09 10:36:37 +0800

    add Guard for dev

--- learn-kotlin/src/main/kotlin/demo/Main.kt
+++ learn-kotlin/src/main/kotlin/demo/Main.kt
@@ -1,6 +1,27 @@
 package demo
 
 fun main(args: Array<String>) {
+    test1()
+}
+
+fun test1() {
+    fun getValue(): String? = "1000d"
+
+    fun parseInt(s: String?): Int? {
+        if (s == null) {
+            return null
+        } else {
+            try {
+                return Integer.parseInt(s)
+            } catch(e: Exception) {
+                println("WARN: ${e.message}")
+                return null
+            }
+        }
+    }
+
+    fun pi(s: String): Int? = parseInt(s)
+
     println("Hello, Kotlin")
     println(parseInt("100"))
     println(parseInt("100x"))
@@ -8,19 +29,3 @@ fun main(args: Array<String>) {
     val s = getValue() as String // hacked
     println(pi(s))
 }
-
-fun getValue(): String? = "1000d"
-
-fun parseInt(s: String?): Int? =
-        if (s == null) {
-            null
-        } else {
-            try {
-                Integer.parseInt(s)
-            } catch(e: Exception) {
-                println("WARN: ${e.message}")
-                null
-            }
-        }
-
-fun pi(s: String): Int? = parseInt(s)
