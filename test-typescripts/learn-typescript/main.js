274e235179b09c4df96a129d6d6eec631e1cd6ae
itang <live.tang@gmail.com>
2016-01-13 11:57:49 +0800

    more

--- test-typescripts/learn-typescript/main.js
+++ test-typescripts/learn-typescript/main.js
@@ -1,55 +1,59 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 //declare var assert: Assert;
 var chai = require("chai");
-var A = chai.assert;
 var assert = chai.assert;
-(function test1() {
+(function test_chai() {
     console.log("hello");
     assert(true);
     assert(true, "true");
     var name = "itang";
-    A.equal(name, "itang");
-    A.equal(name, "itang", "name equals");
-    A.typeOf(name, 'string');
-    A.typeOf(name, "string");
-    A.lengthOf(name, 5);
-    A.lengthOf([1, 2, 3], 3);
+    assert.equal(name, "itang");
+    assert.equal(name, "itang", "name equals");
+    assert.typeOf(name, 'string');
+    assert.typeOf(name, "string");
+    assert.lengthOf(name, 5);
+    assert.lengthOf([1, 2, 3], 3);
     var tea = { "flavors": [1, 2, 3, 4, 5] };
-    A.lengthOf(tea.flavors, 5);
-    A.isFunction(function () { });
-    A.isObject({});
-    A.isNotFunction({});
-    A.isNotObject(function () { });
-    A.throws(function () {
+    assert.lengthOf(tea.flavors, 5);
+    assert.isFunction(function () { });
+    assert.isObject({});
+    assert.isNotFunction({});
+    assert.isNotObject(function () { });
+    assert.throws(function () {
         throw "Error";
     });
 })();
-(function BasicTypes() {
+(function test_basic_types() {
     //Boolean
     (function Boolean() {
         var isDone = true;
-        A.isBoolean(isDone);
-        A.isTrue(isDone);
+        assert.isBoolean(isDone);
+        assert.isTrue(isDone);
         isDone = false;
-        A.isNotTrue(isDone);
+        assert.isNotTrue(isDone);
         //isDone = 1; // compile error
     })();
     //Number
-    (function number() {
+    (function test_number() {
         // all numbers in TypeScript are floating point values. These floating point numbers get the type 'number'.
         var height = 100;
-        A.equal(height, 100);
+        assert.equal(height, 100);
     })();
-    (function String() {
+    (function test_string() {
         var name = "bob";
         name = 'smith';
     })();
     (function test_array() {
         var list = [1, 2, 3];
-        A.isArray(list);
+        assert.isArray(list);
         var list2 = [1, 2, 3];
-        A.isArray(list2);
-        A.notEqual(list2, [1, 2, 3]);
-        A.sameMembers(list2, [1, 2, 3]);
+        assert.isArray(list2);
+        assert.notEqual(list2, [1, 2, 3]);
+        assert.sameMembers(list2, [1, 2, 3]);
     })();
     (function test_enum() {
         var Color;
@@ -60,8 +64,8 @@ var assert = chai.assert;
         })(Color || (Color = {}));
         ;
         var c = Color.Green;
-        A.equal(c, Color.Green);
-        A.equal(Color[2], "Blue");
+        assert.equal(c, Color.Green);
+        assert.equal(Color[2], "Blue");
         var Color2;
         (function (Color2) {
             Color2[Color2["Red"] = 1] = "Red";
@@ -70,24 +74,201 @@ var assert = chai.assert;
         })(Color2 || (Color2 = {}));
         ;
         var colorName = Color2[2];
-        A.equal(colorName, "Green");
+        assert.equal(colorName, "Green");
     })();
     (function test_any() {
         var notSure = 4;
-        A.equal(notSure, 4);
+        assert.equal(notSure, 4);
         notSure = "maybe a string instead";
         notSure = false; // okay, definitely a boolean
-        A.isNotTrue(notSure);
+        assert.isNotTrue(notSure);
         var list = [1, true, "free"];
         list[1] = 100;
-        A.isArray(list);
-        A.equal(list[1], 100);
+        assert.isArray(list);
+        assert.equal(list[1], 100);
     })();
     (function test_void() {
         var f = function () {
             console.log("test");
         };
         var a = f();
-        A.isUndefined(a);
+        assert.isUndefined(a);
     })();
+})();
+(function Interfaces() {
+    function printLabel(labelledObj) {
+        console.log(labelledObj.label);
+    }
+    var myObj = { size: 10, label: "Size 10 Object" };
+    printLabel(myObj);
+    function printLabel2(labelledObj) {
+        console.log(labelledObj.label);
+    }
+    printLabel2(myObj);
+    function createSquare(config) {
+        var newSquare = { color: "white", area: 100 };
+        if (config.color) {
+            newSquare.color = config.color;
+        }
+        if (config.width) {
+            newSquare.area = config.width;
+        }
+        return newSquare;
+    }
+    var mySquare = createSquare({ color: 'black' });
+    console.log(mySquare);
+    var mySearch;
+    mySearch = function (source, subString) {
+        var result = source.search(subString);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
+    assert.isTrue(mySearch("hello, world", "hello"));
+    var myArray;
+    myArray = ["Bob", "Fred"];
+    assert.equal(myArray[0], "Bob");
+    var Clock = (function () {
+        function Clock(h, m) {
+        }
+        Clock.prototype.setTime = function (d) {
+            this.currentTime = d;
+        };
+        return Clock;
+    })();
+    var clock = new Clock(12, 12);
+    clock.setTime(new Date());
+    var ClockClazz = Clock;
+    var newClock = new ClockClazz(1, 30);
+    var square = {};
+    square.color = "blue";
+    square.sideLength = 10;
+    square.penWidth = 5.0;
+    assert.equal(square.color, "blue");
+    /*class CounterImpl implements Counter {
+         (start:number): string {
+            return "";
+        }
+        interval: number = 10;
+        reset(): void {
+
+        }
+    }
+
+    var c: Counter;
+    c(10);
+    c.reset();
+    c.interval = 5.0;
+    */
+})();
+// Classes.
+(function test_classes() {
+    var Greeter = (function () {
+        function Greeter(message) {
+            this.greeting = message;
+        }
+        Greeter.prototype.greet = function () {
+            return "Hello, " + this.greeting;
+        };
+        return Greeter;
+    })();
+    var greeter = new Greeter("world");
+    console.log(greeter.greet());
+    // Inheritance
+    var Animal = (function () {
+        function Animal(theName) {
+            this.name = theName;
+        }
+        // Public by default
+        Animal.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 0; }
+            console.log(this.name + " moved " + meters + "m.");
+        };
+        return Animal;
+    })();
+    var Snake = (function (_super) {
+        __extends(Snake, _super);
+        function Snake(name) {
+            _super.call(this, name);
+        }
+        Snake.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 5; }
+            console.log("Slithering...");
+            _super.prototype.move.call(this, meters);
+        };
+        return Snake;
+    })(Animal);
+    var Horse = (function (_super) {
+        __extends(Horse, _super);
+        function Horse(name) {
+            _super.call(this, name);
+        }
+        Horse.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 45; }
+            console.log("Galloping...");
+            _super.prototype.move.call(this, meters);
+        };
+        return Horse;
+    })(Animal);
+    var sam = new Snake("Sammy the Python");
+    var tom = new Horse("Tommy the Palomino");
+    sam.move();
+    tom.move(34);
+    // The keywords 'public' and 'private' also give you a shorthand for creating and initializing members of your class, by creating parameter properties
+    var Animal2 = (function () {
+        function Animal2(name) {
+            this.name = name;
+        }
+        Animal2.prototype.move = function (meters) {
+            console.log(this.name + " moved " + meters + "m.");
+        };
+        return Animal2;
+    })();
+    // Accessors
+    var passcode = "secret passcode";
+    var Employee = (function () {
+        function Employee() {
+        }
+        Object.defineProperty(Employee.prototype, "fullName", {
+            get: function () {
+                return this._fullName;
+            },
+            set: function (newName) {
+                if (passcode && passcode == "secret passcode") {
+                    this._fullName = newName;
+                }
+                else {
+                    alert("Error: Unauthorized update of employee!");
+                }
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Employee;
+    })();
+    var employee = new Employee();
+    employee.fullName = "Bob Smith";
+    if (employee.fullName) {
+        console.log(employee.fullName);
+    }
+    // Static Properties
+    var Grid = (function () {
+        function Grid(scale) {
+            this.scale = scale;
+        }
+        Grid.prototype.calculateDistanceFromOrigin = function (point) {
+            var xDist = (point.x - Grid.origin.x);
+            var yDist = (point.y - Grid.origin.y);
+            return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+        };
+        Grid.origin = { x: 0, y: 0 };
+        return Grid;
+    })();
+    var grid1 = new Grid(1.0); // 1x scale
+    var grid2 = new Grid(5.0); // 5x scale
+    console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+    console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
 })();
--- test-typescripts/learn-typescript/main.ts
+++ test-typescripts/learn-typescript/main.ts
@@ -4,8 +4,10 @@ declare function require(name: string);
  * @see http://chaijs.com/api/assert/
  */
 interface Assert {
-     //assert(expression: boolean): void;
-     //assert(expression: boolean, message: string): void;
+     // Function interface
+     // describe a function type with an interface
+     (expression: boolean): void;
+     (expression: boolean, message: string): void;
 
      equal(a: any, b: any): void;
      equal(a: any, b: any, message: string): void;
@@ -73,96 +75,98 @@ interface Assert {
      sameMembers(a1: Array<any>, a2: Array<any>, message: string): void;
 }
 
+interface Chai {
+    assert: Assert
+}
 
 //declare var assert: Assert;
-let chai = require("chai");
-let A: Assert /*| ((expr: boolean) => void) | ((expr: boolean, message: string) => void) */ = chai.assert;
+let chai: Chai = require("chai");
 let assert = chai.assert;
 
-(function test1(){
+(function test_chai(){
     console.log("hello");
     assert(true);
     assert(true, "true");
 
     let name = "itang";
-    A.equal(name, "itang");
-    A.equal(name, "itang", "name equals");
+    assert.equal(name, "itang");
+    assert.equal(name, "itang", "name equals");
 
-    A.typeOf(name, 'string');
-    A.typeOf(name, "string");
+    assert.typeOf(name, 'string');
+    assert.typeOf(name, "string");
 
-    A.lengthOf(name, 5);
-    A.lengthOf([1,2,3], 3);
+    assert.lengthOf(name, 5);
+    assert.lengthOf([1,2,3], 3);
     let tea = {"flavors": [1,2,3,4,5]};
-    A.lengthOf(tea.flavors, 5);
+    assert.lengthOf(tea.flavors, 5);
 
-    A.isFunction(function(){});
-    A.isObject({});
-    A.isNotFunction({});
-    A.isNotObject(function(){});
+    assert.isFunction(function(){});
+    assert.isObject({});
+    assert.isNotFunction({});
+    assert.isNotObject(function(){});
 
-    A.throws(function(){
+    assert.throws(function(){
         throw "Error";
     });
 })();
 
-(function BasicTypes(){
+(function test_basic_types(){
     //Boolean
     (function Boolean(){
         var isDone: boolean = true;
-        A.isBoolean(isDone);
-        A.isTrue(isDone);
+        assert.isBoolean(isDone);
+        assert.isTrue(isDone);
         isDone = false;
-        A.isNotTrue(isDone);
+        assert.isNotTrue(isDone);
 
         //isDone = 1; // compile error
     })();
 
     //Number
-    (function number(){
+    (function test_number(){
         // all numbers in TypeScript are floating point values. These floating point numbers get the type 'number'.
         var height: number = 100;
-        A.equal(height, 100);
+        assert.equal(height, 100);
     })();
 
-    (function String(){
+    (function test_string(){
         var name: string = "bob";
         name = 'smith';
     })();
 
     (function test_array(){
         var list: number[] = [1, 2, 3];
-        A.isArray(list);
+        assert.isArray(list);
         var list2 = [1, 2, 3];
-        A.isArray(list2);
-        A.notEqual(list2, [1,2,3]);
-        A.sameMembers(list2, [1,2,3]);
+        assert.isArray(list2);
+        assert.notEqual(list2, [1,2,3]);
+        assert.sameMembers(list2, [1,2,3]);
     })();
 
     (function test_enum(){
         enum Color {Red, Green, Blue};
         var c: Color = Color.Green;
-        A.equal(c, Color.Green);
-        A.equal(Color[2], "Blue");
+        assert.equal(c, Color.Green);
+        assert.equal(Color[2], "Blue");
 
         enum Color2 {Red = 1, Green, Blue};
         var colorName: string = Color2[2];
-        A.equal(colorName, "Green");
+        assert.equal(colorName, "Green");
     })();
 
     (function test_any(){
         var notSure: any = 4;
-        A.equal(notSure, 4);
+        assert.equal(notSure, 4);
 
         notSure = "maybe a string instead";
         notSure = false; // okay, definitely a boolean
-        A.isNotTrue(notSure);
+        assert.isNotTrue(notSure);
 
         var list:any[] = [1, true, "free"];
 
         list[1] = 100;
-        A.isArray(list);
-        A.equal(list[1], 100);
+        assert.isArray(list);
+        assert.equal(list[1], 100);
     })();
 
     (function test_void(){
@@ -170,6 +174,228 @@ let assert = chai.assert;
           console.log("test");
         };
         var a = f();
-        A.isUndefined(a);
+        assert.isUndefined(a);
     })();
+})();
+
+
+(function Interfaces(){
+    function printLabel(labelledObj: {label: string}) {
+        console.log(labelledObj.label);
+    }
+
+    var myObj = {size: 10, label: "Size 10 Object"};
+    printLabel(myObj);
+
+    interface LabelledValue {
+        label: string;
+    }
+
+    function printLabel2(labelledObj: LabelledValue) {
+        console.log(labelledObj.label);
+    }
+    printLabel2(myObj);
+
+    interface SquareConfig {
+        color?: string;
+        width?: number;
+    }
+
+    function createSquare(config: SquareConfig): {color: string, area: number} {
+        var newSquare = {color: "white", area: 100};
+        if (config.color){
+            newSquare.color = config.color;
+        }
+        if(config.width){
+            newSquare.area = config.width;
+        }
+        return newSquare;
+    }
+
+    var mySquare = createSquare({color: 'black'});
+    console.log(mySquare);
+
+    // Function interface
+    interface SearchFunc {
+        (source: string, subString: string): boolean;
+    }
+
+    var mySearch: SearchFunc;
+    mySearch = function(source: string, subString: string) {
+        var result = source.search(subString);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    assert.isTrue(mySearch("hello, world", "hello"));
+
+    // Array Types
+    interface StringArray {
+        [index: number]: string;
+    }
+
+    var myArray: StringArray;
+    myArray = ["Bob", "Fred"];
+    assert.equal(myArray[0], "Bob");
+
+    // Class Types
+    interface ClockInterface {
+        currentTime: Date;
+        setTime(d: Date);
+    }
+
+    class Clock implements ClockInterface  {
+        currentTime: Date;
+        setTime(d: Date) {
+            this.currentTime = d;
+        }
+        constructor(h: number, m: number) { }
+    }
+    var clock = new Clock(12, 12);
+    clock.setTime(new Date());
+
+    interface ClockStatic {
+        new (hour: number, minute: number);
+    }
+
+    var ClockClazz: ClockStatic = Clock;
+    var newClock = new ClockClazz(1, 30);
+
+    // Extending Interfaces
+    interface Shape {
+        color: string;
+    }
+    interface PenStroke {
+        penWidth: number;
+    }
+    interface Square extends Shape, PenStroke {
+        sideLength: number;
+    }
+    var square = <Square>{};
+    square.color = "blue";
+    square.sideLength = 10;
+    square.penWidth = 5.0;
+    assert.equal(square.color, "blue");
+
+    // Hybrid Types
+    interface Counter {
+        (start: number): string;
+        interval: number;
+        reset():void;
+    }
+    /*class CounterImpl implements Counter {
+         (start:number): string {
+            return "";
+        }
+        interval: number = 10;
+        reset(): void {
+
+        }
+    }
+
+    var c: Counter;
+    c(10);
+    c.reset();
+    c.interval = 5.0;
+    */
+})();
+
+// Classes.
+(function test_classes(){
+    class Greeter {
+        greeting: string;
+        constructor(message: string)  {
+            this.greeting = message;
+        }
+        greet() {
+            return "Hello, " + this.greeting;
+        }
+    }
+
+    var greeter = new Greeter("world");
+    console.log(greeter.greet());
+
+    // Inheritance
+    class Animal {
+        private name: string;
+        constructor(theName: string) {
+            this.name = theName;
+        }
+        // Public by default
+        move(meters: number = 0){
+            console.log(this.name + " moved " + meters + "m.");
+        }
+    }
+
+    class Snake extends Animal {
+        constructor(name: string) { super(name);}
+        move(meters = 5){
+            console.log("Slithering...");
+            super.move(meters);
+        }
+    }
+
+    class Horse extends Animal {
+        constructor(name: string) { super(name);}
+        move(meters = 45){
+            console.log("Galloping...");
+            super.move(meters);
+        }
+    }
+
+    var sam = new Snake("Sammy the Python");
+    var tom: Animal = new Horse("Tommy the Palomino");
+
+    sam.move();
+    tom.move(34);
+
+    // The keywords 'public' and 'private' also give you a shorthand for creating and initializing members of your class, by creating parameter properties
+    class Animal2 {
+        constructor(private name: string) {}
+        move(meters: number) {
+            console.log(this.name + " moved " + meters + "m.");
+        }
+    }
+
+    // Accessors
+    var passcode = "secret passcode";
+    class Employee {
+        private _fullName: string;
+        get fullName(): string {
+            return this._fullName;
+        }
+        set fullName(newName: string) {
+            if(passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }else{
+                alert("Error: Unauthorized update of employee!");
+            }
+        }
+    }
+    var employee = new Employee();
+    employee.fullName = "Bob Smith";
+    if(employee.fullName){
+        console.log(employee.fullName);
+    }
+
+    // Static Properties
+    class Grid {
+        static origin = {x: 0, y: 0};
+        calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+            var xDist = (point.x - Grid.origin.x);
+            var yDist = (point.y - Grid.origin.y);
+            return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+        }
+        constructor (public scale: number) { }
+    }
+
+    var grid1 = new Grid(1.0);  // 1x scale
+    var grid2 = new Grid(5.0);  // 5x scale
+
+    console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+    console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
 })();
