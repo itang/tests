5ad9656d780c2ce7171d8e2165e92a23864cec38
itang <live.tang@gmail.com>
2016-01-26 17:44:59 +0800

    more

--- test-kotlins/test-jwt/src/main/kotlin/demo/Main.kt
+++ test-kotlins/test-jwt/src/main/kotlin/demo/Main.kt
@@ -3,20 +3,21 @@ package demo
 import io.jsonwebtoken.Jwts
 
 import authentikat.jwt.*
+import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.impl.crypto.MacProvider
 import java.util.*
 
 fun main(args: Array<String>) {
-
-    val key = MacProvider.generateKey()
+    val key = MacProvider.generateKey(SignatureAlgorithm.HS256)
+    println(">>key.algorithm ${key.algorithm}")
     val keyBase64EncodedString = Base64.getUrlEncoder().encodeToString(key.encoded)
     println(">>GenerateKey: $keyBase64EncodedString")
 
     ////////////////////////////////////////////////////
-    val prodKey = "hzz/XCah+YVk6jpcjDBLOUrLhM47asAbkJ3aCZpQmgiIZPHiZknR051CTpK/X6f2h1pC4cZEo1eXuFuJBEQ4YA=="
-    val token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdXN0b21lcjFAZXhhbXBsZS5jb20iLCJzZXNzaW9uX2N0eCI6eyJ1c2VyaWQiOjMsInVzZXJuYW1lIjoiY3VzdG9tZXIxQGV4YW1wbGUuY29tIiwiZnVsbG5hbWUiOm51bGwsInBlcm1zIjpbXSwicm9sZXMiOltdfSwiaWF0IjoxNDUzNzg2MDUzfQ.KZfU-ldt3OKiNUWsUIW7bpTRzOnNO5MxDpZqFaUxuZg"
+    val prodKey = "uLkvkYvgiA01ozKoTvyyXL_YBZUxDQK0OGosXmdBg84="
+    val token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdXN0b21lcjFAZXhhbXBsZS5jb20iLCJzZXNzaW9uX2N0eCI6eyJ1c2VyaWQiOjMsInVzZXJuYW1lIjoiY3VzdG9tZXIxQGV4YW1wbGUuY29tIiwiZnVsbG5hbWUiOm51bGwsInBlcm1zIjpbXSwicm9sZXMiOltdfSwiaWF0IjoxNDUzODAxMjczfQ.iwghjrT2efWaXltTuig-mRN7jMcQUOdLePuWozVedms"
 
-    val claims = Jwts.parser().setSigningKey(prodKey).parseClaimsJws(token).getBody()
+    val claims = Jwts.parser().setSigningKey(prodKey).parseClaimsJws(token).body
     println(">>Claims by Jwts: $claims")
 
     val isValid = JsonWebToken.validate(token, prodKey) // why false ??
