bf7e42d18ad46a44644a050fbf49fd880c64ed97
itang <live.tang@gmail.com>
2016-02-03 23:53:03 +0800

    cleanup

--- test-rusts/test-mem-stack/src/main.rs
+++ test-rusts/test-mem-stack/src/main.rs
@@ -2,8 +2,9 @@ use std::mem;
 use std::thread;
 
 
-const SIZE_FN: usize = 1024 * 1021 * 8; // ~8M
+const SIZE_FN: usize = 1024 * 1020 * 8; // ~8M
 const SIZE_TH: usize = 1024 * 1020 * 2; // ~2M
+const SIZE_OT: usize = 1024 * 1024 * 6; // 100M for heap
 
 fn main() {
     let r0 = 1i32;
@@ -25,6 +26,21 @@ fn main() {
 
     test_fn_stack_size();
     test_thread_stack_size();
+
+    println!("try test heap...");
+    test_heap();
+}
+
+fn test_heap() {
+    println!("Heap happy???");
+    // @see:
+    // stackoverflow.com/questions/30242770/how-to-allocate-arrays-on-the-heap-in-rust-1-0-beta
+    // ERROR
+    // let _it = Box::new(S_HEAP{a: Box::new([0; SIZE_OT])});
+    // ERROR
+    use std::iter::repeat;
+    const SIZE: usize = SIZE_OT;
+    let _v: Vec<_> = repeat(10.0).take(SIZE).collect();
 }
 
 fn test_fn_stack_size() {
@@ -58,4 +74,11 @@ struct S_FN {
 
 struct S_TH {
     a: [i8; SIZE_TH],
+}
+
+struct S_HEAP {
+    a: Box<[i8; SIZE_OT]>, /* The problem is that the array is being passed
+                            * to the Box::new function as an argument,
+                            * which means it has to be created first,
+                            * which means it has to be created on the stack */
 }
