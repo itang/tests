2e6bdf6d661bb2472d041eebb7847303a0aa809e
itang <live.tang@gmail.com>
2016-02-19 10:01:02 +0800

    update

--- test-crystals/test-frost/db/migrations/000001_create_posts.cr
+++ test-crystals/test-frost/db/migrations/000001_create_posts.cr
@@ -1,6 +1,8 @@
 # db/migrations/000001_create_posts.cr
 
 class CreatePosts < Frost::Record::Migration
+  set_version {{ __FILE__ }}
+
   up do
     create_table :posts do |t|
       t.string :title
--- test-crystals/test-frost/db/structure.sql
+++ test-crystals/test-frost/db/structure.sql
@@ -30,12 +30,59 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: posts; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE posts (
+    id integer NOT NULL,
+    title character varying(255),
+    body text,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE posts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: posts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE posts_id_seq OWNED BY posts.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE schema_migrations (
     version character varying(14) NOT NULL
 );
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY posts ALTER COLUMN id SET DEFAULT nextval('posts_id_seq'::regclass);
+
+
+--
+-- Name: posts_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
 
 
 --
@@ -50,3 +97,4 @@ ALTER TABLE ONLY schema_migrations
 -- PostgreSQL database dump complete
 --
 
+INSERT INTO schema_migrations (version) VALUES ('000001');
