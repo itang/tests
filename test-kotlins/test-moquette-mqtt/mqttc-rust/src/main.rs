718f9092ec073985b60e4bec717de9944d388bef
itang <live.tang@gmail.com>
2016-04-23 21:28:45 +0800

    add mqttc-rust client test

---
+++ test-kotlins/test-moquette-mqtt/mqttc-rust/src/main.rs
@@ -0,0 +1,28 @@
+extern crate mqttc;
+extern crate netopt;
+
+use mqttc::*;
+use netopt::*;
+use std::time::Duration;
+
+fn main() {
+    let netopt = NetworkOptions::new();
+    let mut opts = ClientOptions::new();
+
+    opts.set_reconnect(ReconnectMethod::ReconnectAfter(Duration::from_secs(1)));
+    let mut client = opts.connect("127.0.0.1:1883", netopt).expect("Can't connect to server");
+
+    client.subscribe("mqtt/test").unwrap();
+    loop {
+        match client.await().unwrap() {
+            Some(message) => {
+                println!("{:?}", message);
+
+                let payload: &Vec<u8> = &*(message.payload.clone());
+                let payload: String = String::from_utf8(payload.clone()).expect("转码出错了");
+                println!("topic:{}, payload:{}", message.topic.path, payload)
+            }
+            None => {}
+        }
+    }
+}
