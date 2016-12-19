431a7e135640996fcdaceaeb458a0ca43a6f8ebf
itang <live.tang@gmail.com>
2016-12-19 20:23:25 +0800

    逐步分摊法

--- test-kotlins/test_fentan/src/main/kotlin/demo/Main.kt
+++ test-kotlins/test_fentan/src/main/kotlin/demo/Main.kt
@@ -3,9 +3,7 @@ package demo
 import java.math.BigDecimal
 
 
-data class OrderItem(val id: Int, val price: Double, val amount: Int) {
-
-}
+data class OrderItem(val id: Int, val price: Double, val amount: Int)
 
 
 fun main(args: Array<String>) {
@@ -33,15 +31,25 @@ fun main(args: Array<String>) {
 
     println()
     test(items5, 1.0)
+
+
+    val items6 = listOf(OrderItem(1, 10.0, 1), OrderItem(2, 10.0, 1), OrderItem(3, 10.0, 1), OrderItem(4, 10.0, 1), OrderItem(5, 10.0, 1), OrderItem(6, 10.0, 1), OrderItem(7, 10.0, 1))
+
+    println()
+    test(items6, 0.05)
 }
 
 
 fun test(items: List<OrderItem>, reduceTotal: Double) {
     test1(items, reduceTotal)
     test2(items, reduceTotal)
+    test3(items, reduceTotal)
 }
 
-
+/**
+ * 存在累积无法放大问题.
+ *
+ */
 fun test1(items: List<OrderItem>, reduceTotal: Double) {
     val total = items.map { it.amount * it.price }.sum()
     var acc = 0.0
@@ -56,24 +64,28 @@ fun test1(items: List<OrderItem>, reduceTotal: Double) {
         val fixedItemReduce = if (!it.hasNext()) { // last
             reduceTotal - acc
         } else {
-            fixed1(rawItemReduce)
+            double_round(rawItemReduce)
         }
         acc += fixedItemReduce
 
-        println("$id-reduce: $rawItemReduce (${fixed1(rawItemReduce)}) <-> $fixedItemReduce : (${fixed1(fixedItemReduce)})")
+        println("$id-reduce: $rawItemReduce (${double_round(rawItemReduce)}) <-> $fixedItemReduce : (${double_round(fixedItemReduce)})")
     }
 }
 
 
+/**
+ * 存在累积无法放大问题.
+ *
+ */
 fun test2(items: List<OrderItem>, reduceTotal: Double) {
-    val total = items.map { it.amount * fixed2(it.price) }.sum()
-    val reduceTotal2 = fixed2(reduceTotal)
+    val total = items.map { it.amount * to_cent_round(it.price) }.sum()
+    val reduceTotal2 = to_cent_round(reduceTotal)
     var acc = 0L
 
     val it = items.iterator()
     while (it.hasNext()) {
         val (id, price, amount) = it.next()
-        val s = amount * fixed2(price)
+        val s = amount * to_cent_round(price)
         val r = s * 1.0 / total
         val reduce0 = r * reduceTotal2
 
@@ -88,13 +100,46 @@ fun test2(items: List<OrderItem>, reduceTotal: Double) {
     }
 }
 
-fun fixed1(value: Double): Double {
+/**
+ * 最靠谱的分摊算法.
+ *
+ */
+fun test3(items: List<OrderItem>, reduceTotal: Double) {
+    var total = items.map { it.amount * to_cent_round(it.price) }.sum()
+    var rt = to_cent_round(reduceTotal)
+
+    val it = items.iterator()
+    while (it.hasNext()) {
+        val (id, price, amount) = it.next()
+        val s = amount * to_cent_round(price)
+        val r = s * 1.0 / total
+        val v = round(r * rt)
+
+        val reduce: Long = if (!it.hasNext()) { // last
+            rt
+        } else {
+            v
+        }
+
+        total -= s
+        rt -= v
+
+        println("$id-reduce: $v  <-> $reduce")
+    }
+}
+
+
+fun double_round(value: Double): Double {
     return BigDecimal(value.toString()).setScale(2, BigDecimal.ROUND_HALF_UP).toDouble()
 
     //http://www.cnblogs.com/xd502djj/archive/2011/07/21/2112683.html
     //return BigDecimal(value).setScale(2, BigDecimal.ROUND_HALF_UP).toDouble() //public BigDecimal(double val) 将 double 转换为 BigDecimal，后者是 double 的二进制浮点值准确的十进制表示形式。返回的 BigDecimal 的标度是使 (10scale × val)为整数的最小值。此构造方法的结果有一定的不可预知性
 }
 
-fun fixed2(value: Double): Long {
+fun to_cent_round(value: Double): Long {
     return Math.round(value * 100)
+}
+
+fun round(value: Double): Long {
+    return Math.round(value)
 }
