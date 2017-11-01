50892de770c397c973132d7af575619e05ae24be
itang <live.tang@gmail.com>
2017-11-01 11:35:48 +0800

    Revert "add test elixir docs doctest"
    This reverts commit bb3fc238f054cbdf7f181c185de857bf3ddd18d6.

--- test-elixirs/testdocs/test/testdocs_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TestdocsTest do
-  use ExUnit.Case
-  doctest Testdocs
-
-  test "greets the world" do
-    assert Testdocs.hello() == :world
-  end
-end
--- test-elixirs/testdocs/test/todo_list_inbox_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TodoList.InboxTest do
-  use ExUnit.Case
-  doctest TodoList.Inbox
-
-  test "returns the header" do
-    assert TodoList.Inbox.header() == "Inbox"
-  end
-end
