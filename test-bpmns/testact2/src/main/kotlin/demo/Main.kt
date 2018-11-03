fd658e410175412285ec7daf195d93630df93597
itang <live.tang@gmail.com>
2018-11-03 13:07:03 +0800

    test activiti 6

---
+++ test-bpmns/testact2/src/main/kotlin/demo/Main.kt
@@ -0,0 +1,50 @@
+package demo
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.activiti.engine.ProcessEngines
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+    try {
+        //创建流程引擎
+        val engine = ProcessEngines.getDefaultProcessEngine()
+        //得到流程存储服务组件
+        val repositoryService = engine.repositoryService
+
+        //获取流程运行时组件
+        val runtimeService = engine.runtimeService
+
+        //获取流程任务组件
+        val taskService = engine.taskService
+
+        //部署流程文件
+        repositoryService.createDeployment().addClasspathResource("bpmn/First.bpmn").deploy()
+
+        //启动流程
+        runtimeService.startProcessInstanceByKey("process1")
+
+        //查询第一个任务
+        val task1 = taskService.createTaskQuery().singleResult()!!
+        println("task1 name:${task1.name}")
+
+        //完成第一个任务
+        taskService.complete(task1.id)
+
+        //查询第二个任务
+        val task2 = taskService.createTaskQuery().singleResult()!!
+        println("task2.name ${task2.name}")
+
+
+        //完成第二个任务
+        taskService.complete(task2.id)
+
+        val moreTask = taskService.createTaskQuery().singleResult()
+        println("more task: $moreTask")
+        System.exit(0)
+        //退出
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
