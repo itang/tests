caf47a9e06a07877171fa4f3db867b75f7050ff9
itang <live.tang@gmail.com>
2018-12-15 21:55:54 +0800

    add test java design pattern - vistor

---
+++ test-java-patterns/simple-visitor2/src/test/java/demo/DemoTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DemoTest {
+    @Test
+    public void testIt() {
+        assertEquals("hello", "hello");
+    }
+}
