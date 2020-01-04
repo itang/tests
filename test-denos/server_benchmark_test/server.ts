c25ac627119083c894ca5d74707e7267cc6e7dc4
itang <live.tang@gmail.com>
2020-01-04 21:41:17 +0800

    add test for deno http server

---
+++ test-denos/server_benchmark_test/server.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@v0.28.1/http/server.ts";
+
+const body = new TextEncoder().encode('Hello World\n');
+const s = serve({ port: 8000 });
+console.log('http://localhost:8000/');
+for await (const req of s) {
+  req.respond({body})
+}
+
