b48b21b07d7c1f5a5d00cd3a6ad72f7ea08cd54a
itang <live.tang@gmail.com>
2020-06-10 01:09:45 +0800

    use Path for path concat

--- test-rusts/testfile/src/main.rs
+++ test-rusts/testfile/src/main.rs
@@ -2,15 +2,20 @@ use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 // Box<dyn Error> to mean “any kind of error.”
 fn main() -> Result<(), Box<dyn Error>> {
-    let path = env::var("HOME")? + "/.zshrc";
+    let path = Path::new(&env::var("HOME")?).join(".zshrc");
+    //let path = env::var("HOME")? + "/.zshrc";
     let mut content = String::new();
     File::open(path)?.read_to_string(&mut content)?;
 
-    println!("{}", content);
-    println!("{}", content.lines().count());
+    println!("lines: {}", content.lines().count());
+    println!("{}", "-".repeat(100));
+    for (i, item) in content.lines().enumerate() {
+        println!("{:3}: {}", i + 1, item);
+    }
 
     Ok(())
 }
